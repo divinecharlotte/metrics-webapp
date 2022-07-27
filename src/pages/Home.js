1a431f55def7b38b4bbd0b81406be7f8531e1dc9
charlotte-divine <divinemaina@gmail.com>
2022-07-27 11:28:10 +0100

    add required changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 import Chart from '../components/Chart';
 import Coin from '../components/Coin';
 import Header from '../components/Header';
@@ -15,27 +15,52 @@ const Home = () => {
     dispatch(FetchCoinsFunc());
   }, []);
 
+  const [search, setSearch] = useSearchParams();
+
   const handleDetails = (id) => navigate(`/details/${id}`);
   return (
     <div data-testid="intro-page" className="data-cont">
       <Header path="/" />
       <Chart title1="Crypto" title2="Currency" count={`${coins.length} Coins`} />
+
       <div>
-        <h3 className="coins-title">Coins By Name, Symbol, Price</h3>
+
+        <input
+          className="coins-title"
+          type="text"
+          value={search.get('filter') || ''}
+          placeholder="Search by Crypto name  eg : BNB"
+          onChange={(e) => {
+            const filter = e.target.value;
+            if (filter) {
+              setSearch({ filter });
+            } else {
+              setSearch({});
+            }
+          }}
+        />
         <div className="coin-card-cont">
-          {coins.map((coin) => (
-            <Coin
-              key={coin.id}
-              name={coin.name}
-              symbol={coin.symbol}
-              id={coin.id}
-              price={coin.priceUsd}
-              handleDetails={handleDetails}
-            />
-          ))}
+          {coins
+            .filter((coin) => {
+              const filter = search.get('filter');
+              if (!filter) return true;
+              const name = coin.name.toLowerCase();
+              return name.startsWith(filter.toLowerCase());
+            })
+            .map((coin) => (
+              <Coin
+                key={coin.id}
+                name={coin.name}
+                symbol={coin.symbol}
+                id={coin.id}
+                price={coin.priceUsd}
+                handleDetails={handleDetails}
+              />
+            ))}
         </div>
       </div>
     </div>
+
   );
 };
 
